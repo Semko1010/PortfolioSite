1dda86d31cd87d2bb187157428c6b6caf9993b4a
Semko1010 <semir01020@gmail.com>
2022-01-09 23:38:29 +0100

    v1.2

--- src/Component/mov/Mov.js
+++ src/Component/mov/Mov.js
@@ -33,9 +33,11 @@ class Mov extends Component {
     render() { 
         console.log(this.state.data);
         return ( <div className="mov">
-        <h2 style={{transform: `translateX(${ this.state.scroll * 0.10}px) `}}><a href="https://the-movie-app-2021.netlify.app/" target="_blank" rel="noreferrer">The Movie App</a></h2>
-        <h3 style={{transform: `translateX(${ this.state.scroll * 0.10}px) `}}>Front-End Developer • Created with React</h3>
-        <p style={{transform: `translateX(${ this.state.scroll * 0.10}px) `}}>Find your favorite movies and enjoy them with your friends</p>
+            <div className="movText" style={{transform: `translateX(${ this.state.scroll * 0.10}px) `}}>
+        <h2 ><a href="https://the-movie-app-2021.netlify.app/" target="_blank" rel="noreferrer">The Movie App</a></h2>
+        <h3 >Front-End Developer • Created with React</h3>
+        <p >Find your favorite movies and enjoy them with your friends</p>
+        </div>
         <img style={{transform: `translateY(${- this.state.scroll * 0.2}px)`}} id="img1" src="./img/mov/Bildschirmfoto 2021-12-29 um 09.59.27.png" alt="" />
         <img  style={{transform: `translateY(${- this.state.scroll * 0.6}px)`}} id="img2" src="./img/mov/Bildschirmfoto 2021-12-29 um 09.58.07.png" alt="" />
         <img  style={{transform: `translateY(${- this.state.scroll * 0.9}px)`}} id="img3" src="./img/mov/Bildschirmfoto 2021-12-29 um 09.56.23.png" alt="" />

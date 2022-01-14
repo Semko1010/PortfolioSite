62cd6c5359df894d77991d3fdd201635f2598d19
Semko1010 <semir01020@gmail.com>
2022-01-14 15:55:29 +0100

    v6.0

--- src/Component/Contact/Contact.js
+++ src/Component/Contact/Contact.js
@@ -48,7 +48,7 @@ class AboutMe extends Component {
         }
     }
            else if(window.matchMedia("(max-width: 1920px)").matches){
-             if(this.state.data >7300 && this.state.data <9100){
+             if(this.state.data >7300 && this.state.data <10100){
             this.setState({scroll:this.state.data})
             console.log("1920");
         }
@@ -83,9 +83,9 @@ class AboutMe extends Component {
         return ( <article className="contact">
                            <h2 style={{transform: `translateX(${-this.state.scroll * 0.5}px)`}}>Contact</h2>
                             <div>
-                        <a href="https://github.com/Semko1010"><img src="./img/contact/github.svg" alt="" style={{transform: `translateX(${this.state.scroll * 0.5}px)`}}/></a>
-                        <a href="https://www.linkedin.com/in/semir-hamidovic/"><img src="./img/contact/linkedin.svg" alt="" style={{transform: `translateX(${this.state.scroll * 0.5}px)`}}/></a> 
-                        <a href="mailto:semir01020@gmail.com"><img src="./img/contact/at-solid.svg" alt="" style={{transform: `translateX(${this.state.scroll * 0.5}px)`}}/></a>   
+                        <a href="https://github.com/Semko1010"><img src="./img/contact/github.svg" alt="" s/></a>
+                        <a href="https://www.linkedin.com/in/semir-hamidovic/"><img src="./img/contact/linkedin.svg" alt="" /></a> 
+                        <a href="mailto:semir01020@gmail.com"><img src="./img/contact/at-solid.svg" alt="" /></a>   
                         </div>
                         </article> );
     }

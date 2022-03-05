bb43d5c1a16ac567bbc2ad619d03db74bdb50eaa
Semko1010 <semir01020@gmail.com>
2022-03-05 08:03:26 +0100

    nothingToTrash

--- src/App.js
+++ src/App.js
@@ -1,61 +1,58 @@
-import Header from "./Component/Header/Header"
-import Mov from "./Component/mov/Mov"
-import AboutMe from "./Component/AboutMe/AbotMe"
-import Tasty from "./Component/tasty/Tasty"
-import Music from "./Component/Music/Music"
-import DesignShop from "./Component/DesignSHop/DesignShop"
-import Est from "./Component/Est/Est"
-import Skills from "./Component/Skills/Skills"
-import Contact from "./Component/Contact/Contact"
+import Header from "./Component/Header/Header";
+import Mov from "./Component/mov/Mov";
+import AboutMe from "./Component/AboutMe/AbotMe";
+import Tasty from "./Component/tasty/Tasty";
+import Music from "./Component/Music/Music";
+import DesignShop from "./Component/DesignSHop/DesignShop";
+import Est from "./Component/Est/Est";
+import Trash from "./Component/Trash/Trash";
+import Skills from "./Component/Skills/Skills";
+import Contact from "./Component/Contact/Contact";
 
-import './App.css';
-
-
+import "./App.css";
 
 function App() {
-  
-window.onbeforeunload = function () {
-window.scrollTo(0,0);
-};
-  return (
-   
-    <div className="App">
-      <main>
-      <div className="header">
-        <Header/>
-      
-      
-      </div>
-      <div className="AboutMeDiv">
-      <AboutMe/>
-      </div>
-      
-      <div className="movDiv">
-      <Mov/>
-      </div>
-      
-      <div className="tastyDiv">
-      <Tasty/>
-      </div>
-      <div className="musicDiv">
-      <Music/>
-      </div>
-      <div className="shopDiv">
-        <DesignShop/>
-      </div>
-      <div className="estDiv">
-        <Est/>
-      </div>
-      <div className="skillsDiv">
-        <Skills/>
-      </div>
-      <div className="contactDiv">
-        <Contact/>
-      </div>
-      </main>
-    </div>
-  );
+	window.onbeforeunload = function () {
+		window.scrollTo(0, 0);
+	};
+	return (
+		<div className='App'>
+			<main>
+				<div className='header'>
+					<Header />
+				</div>
+				<div className='AboutMeDiv'>
+					<AboutMe />
+				</div>
+
+				<div className='movDiv'>
+					<Mov />
+				</div>
+
+				<div className='tastyDiv'>
+					<Tasty />
+				</div>
+				<div className='musicDiv'>
+					<Music />
+				</div>
+				<div className='shopDiv'>
+					<DesignShop />
+				</div>
+				<div className='estDiv'>
+					<Est />
+				</div>
+				<div className='trashDiv'>
+					<Trash />
+				</div>
+				<div className='skillsDiv'>
+					<Skills />
+				</div>
+				<div className='contactDiv'>
+					<Contact />
+				</div>
+			</main>
+		</div>
+	);
 }
 
 export default App;
-
---
+++ src/Component/Trash/Trash.js
@@ -0,0 +1,77 @@
+import React, { Component } from "react";
+import "./Trash.css";
+
+class Music extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			scroll: 0,
+		};
+	}
+
+	handleScroll1 = () => {
+		let el = document.querySelector(".trash");
+		let elPosition = el.getBoundingClientRect();
+		let screenPosition = window.innerHeight;
+
+		if (elPosition.top + 100 < screenPosition) {
+			this.setState({ scroll: window.pageYOffset });
+			document.querySelector(".trashText").style =
+				"visibility:visible;opacity:1;transform: translateX(0);";
+		}
+		if (elPosition.top > screenPosition) {
+			document.querySelector(".trashText").style =
+				"visibility:hidden;opacity:0";
+		}
+		if (elPosition.bottom < screenPosition) {
+			document.querySelector(".trashText").style =
+				"visibility:hidden;opacity:0";
+		}
+	};
+
+	componentDidMount() {
+		window.addEventListener("scroll", this.handleScroll1);
+		window.addEventListener("scroll", this.handleScroll);
+	}
+
+	render() {
+		return (
+			<div className='trash'>
+				<div className='trashText'>
+					<h2>Nothing to Trash</h2>
+					<h3>Full-Stack Developer • Created with Reactjs/Nodejs</h3>
+					<p>Sell your second hand stuff</p>
+					<a
+						href='https://nothing2trash.herokuapp.com/'
+						target='_blank'
+						rel='noreferrer'
+						class='est-flip'
+						data-back='Calculator'
+						data-front='Show Me'>
+						{" "}
+					</a>
+				</div>
+				<img
+					style={{ transform: `translateY(${-this.state.scroll * 0.2}px)` }}
+					id='img1'
+					src='./img/Trash/iphone-1766253.png'
+					alt=''
+				/>
+				<img
+					style={{ transform: `translateY(${-this.state.scroll * 0.6}px)` }}
+					id='img2'
+					src='/img/Trash/ipad-147691.png'
+					alt=''
+				/>
+				<img
+					style={{ transform: `translateY(${-this.state.scroll * 0.9}px)` }}
+					id='img3'
+					src='/img/Trash/macbook-562499.png'
+					alt=''
+				/>
+			</div>
+		);
+	}
+}
+
+export default Music;
